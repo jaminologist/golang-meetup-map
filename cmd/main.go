cb9fd6c804c3725ced7079523359c68484f9e7a3
Benjamin Bryant <benjaminabryante@gmail.com>
2022-09-04 07:30:18 +0100

    added template. generates map markers via csv

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,62 @@
 package main
 
-import "fmt"
+import (
+	"bytes"
+	_ "embed"
+	"encoding/csv"
+	"log"
+	"os"
+	"text/template"
+)
+
+//go:embed meetups.csv
+var b []byte
 
 func main() {
-	fmt.Println("Hello World")
+	csvReader := csv.NewReader(bytes.NewReader(b))
+	records, _ := csvReader.ReadAll()
+	meetups := recordsToMeetups(records[1:])
+	parse("./template/index.html", meetups)
+}
+
+type Meetup struct {
+	Name      string
+	Link      string
+	Latitude  string
+	Longitude string
+}
+
+func recordsToMeetups(records [][]string) []Meetup {
+	meetups := make([]Meetup, 0)
+	for _, record := range records {
+		meetup := Meetup{
+			Name:      record[0],
+			Link:      record[1],
+			Latitude:  record[2],
+			Longitude: record[3],
+		}
+		meetups = append(meetups, meetup)
+	}
+	return meetups
+}
+
+func parse(path string, meetups []Meetup) {
+	t, err := template.ParseFiles(path)
+	if err != nil {
+		log.Print(err)
+		return
+	}
+
+	f, err := os.Create("../docs/index.html")
+	if err != nil {
+		log.Println("create file: ", err)
+		return
+	}
+
+	err = t.Execute(f, meetups)
+	if err != nil {
+		log.Print("execute: ", err)
+		return
+	}
+	f.Close()
 }
--- docs/index.js
+++
@@ -1,9 +0,0 @@
-    var map = L.map('map').setView([51.505, -0.09], 13);
-
-	var tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-		maxZoom: 19,
-		attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-	}).addTo(map);
-
-	var marker = L.marker([51.5435, -0.09]).addTo(map)
-		.bindPopup('<b>Hello world!</b><br />I am a popup.').openPopup();
